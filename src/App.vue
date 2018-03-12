f250b01f6a43ac472a4bb738b12cb35c4a1c3825
Jose Torreblanca <josewhitetower@gmail.com>
2018-03-12 18:38:21 +0100

    deploying to gh-pages

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,10 @@
 <template>
+<div>
     <Calculator/>
+    <p class="footer">Created with love by <a href="https://github.com/josewhitetower" target="_blank"> @josewhitetower</a></p>
+    <p class="footer">Inspired by <a href="https://codepen.io/mjijackson/pen/xOzyGX" target="_blank">Michael Jackson's React Calculator</a></p>
+    <p class="footer">Source code on  <a href="https://github.com/josewhitetower/calculator" target="_blank">Github</a></p>
+</div>
 </template>
 
 <script>
@@ -14,5 +19,8 @@ export default {
 </script>
 
 <style>
-
+.footer{
+  text-align: center;
+  font-family: 'Courier New', Courier, monospace;
+}
 </style>
